eea57b78f9f909b2f2c9b1e199b6d933b69ea729
nd0ut <nd0ut.me@gmail.com>
2013-10-15 09:48:01 +0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+OctoCrowd::Application.config.secret_key_base = 'd381b11ba5bccd5314b98558b46c8dbb9a4d61935aa39b94b87cf92a9ec89d59df5832ae86d0724382ef6eba6e8b83a89b9af1b1e1d96c6ebe2433183c145917'
