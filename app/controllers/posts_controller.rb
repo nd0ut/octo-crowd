aad455288c8f67a994a2304d60b37c77314e5aaf
nd0ut <nd0ut.me@gmail.com>
2013-10-17 02:15:40 +0400

    creating and showing posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,4 +2,20 @@ class PostsController < ApplicationController
   def new
     @post = Post.new
   end
+
+  def create
+    @post = Post.new(post_params)
+    @post.save
+
+    redirect_to @post
+  end
+
+  def show
+    @post = Post.find(params[:id])
+  end
+
+  private
+  def post_params
+    params.require(:post).permit(:title, :body)
+  end
 end
