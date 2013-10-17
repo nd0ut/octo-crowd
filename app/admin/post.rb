b764539c1bfac02b5285baebb1abf401064245c8
nd0ut <nd0ut.me@gmail.com>
2013-10-17 23:09:24 +0400

    add moderation action to activeadmin

--- app/admin/post.rb
+++ app/admin/post.rb
@@ -1,3 +1,26 @@
 ActiveAdmin.register Post do
+  filter :moderation_state, as: :select
 
+  index do
+    column :title
+    column :created_at
+    default_actions
+
+    actions do |post|
+      link_to "Accept", accept_admin_post_path(post), :class => "member_link", method: :put
+      link_to "Reject", reject_admin_post_path(post), :class => "member_link", method: :put
+    end
+  end
+
+  member_action :accept, :method => :put do
+    post = Post.find(params[:id])
+    post.accept
+    redirect_to :action => :show, :notice => "Accepted!"
+  end
+
+  member_action :reject, :method => :put do
+    post = Post.find(params[:id])
+    post.reject
+    redirect_to :action => :show, :notice => "Rejected!"
+  end
 end
