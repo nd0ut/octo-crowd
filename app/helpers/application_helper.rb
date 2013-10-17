bcb7e6df688caecc9403773f32d429b7d73a62b6
nd0ut <nd0ut.me@gmail.com>
2013-10-17 16:49:57 +0400

    refactor

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -5,4 +5,13 @@ module ApplicationHelper
 
     provider.underscore.humanize.downcase
   end
+
+  def icon_with_label(label, icon_class)
+    "<i class=\"#{icon_class}\"></i>&nbsp; &nbsp;#{label}</a>".html_safe
+  end
+
+  def label_with_icon(label, icon_class)
+    "#{label}&nbsp;&nbsp;<i class=\"#{icon_class}\"></i>".html_safe
+  end
+
 end
