93aded8d1b92ee6d79b7a95f2d16adeb3e873d75
nd0ut <nd0ut.me@gmail.com>
2013-10-18 00:03:03 +0400

    fix Post strong parameters

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -16,6 +16,6 @@ class PostsController < ApplicationController
 
   private
   def post_params
-    params.require(:post).permit(:title, :body, :category_ids)
+    params.require(:post).permit(:title, :body, category_ids: [])
   end
 end
