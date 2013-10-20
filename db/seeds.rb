791dd6101344b8ec5476db8923cc6d0d54c9eec0
nd0ut <nd0ut.me@gmail.com>
2013-10-20 13:07:59 +0400

    seed posts

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,3 +3,11 @@ Category.create(name: 'health')
 Category.create(name: 'lifehacks')
 Category.create(name: 'tech')
 Category.create(name: 'sport')
+
+# User
+User.create(username: 'nd0ut', admin: true, email: 'nd0ut.me@gmail.com', password: 'qweqweqwe')
+
+# Posts
+30.times do
+  User.last.posts.create(title: 'test', body: 'test', moderation_state: 'accepted')
+end
