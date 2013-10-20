58b697c375e737d53c0740e6747de1e9e2819737
nd0ut <nd0ut.me@gmail.com>
2013-10-20 12:30:10 +0400

    post title validation

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,6 +4,10 @@ class Post < ActiveRecord::Base
   belongs_to :author, class_name: 'User'
   has_and_belongs_to_many :categories
 
+  validates :title, presence: true,
+                    allow_blank: false,
+                    length: { minimum: 3, maximum: 140 }
+
   state_machine :moderation_state, :initial => :waiting do
     state :waiting
     state :accepted
