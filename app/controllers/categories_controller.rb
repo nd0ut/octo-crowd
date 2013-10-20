6e5a73f35781f8e19708e17f04960ed455790bd6
nd0ut <nd0ut.me@gmail.com>
2013-10-20 09:55:24 +0400

    Revert "pretty category url (working version)"
    This reverts commit 1703a3f4bf8c9e1fa9c4a117fb08cbe6dd38e3c1.

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,6 +1,5 @@
 class CategoriesController < ApplicationController
   def show
-    # params[:id] is the name of the cotegory, not the id
-    @posts = Category.where(name: params[:id]).first.posts
+    @posts = Category.find(params[:id]).posts
   end
 end
