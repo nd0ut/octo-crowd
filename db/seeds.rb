e4269a0546c1827180b2a31b9db981a88d0ebe56
nd0ut <nd0ut.me@gmail.com>
2013-10-20 10:33:13 +0400

    create category seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,5 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+# Categories
+Category.create(name: 'health')
+Category.create(name: 'lifehacks')
+Category.create(name: 'tech')
+Category.create(name: 'sport')
