930b09e0e85b0c67a5366455972e9116087fd904
nd0ut <nd0ut.me@gmail.com>
2013-10-24 00:45:17 +0400

    fuck yeah. it works

--- config/deploy.rb
+++ config/deploy.rb
@@ -13,11 +13,13 @@ set :pty, true
 # set :linked_files, %w{config/database.yml}
 set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
 
-set :default_env, { path: "/usr/local/rvm/bin:$PATH" }
+# set :default_env, { path: "/usr/local/rvm/bin:$PATH" }
 set :keep_releases, 1
 
-# set :rvm_type, :auto
+set :rvm_type, :user
 # set :rvm_ruby_version, '2.0.0@rails4'
+set :rvm_custom_path, '/home/rails/.rvm'
+set :bundle_cmd, '/home/rails/.rvm/bin/OctoCrowd_bundle'
 
 before 'unicorn:start', 'rvm:hook'
 
