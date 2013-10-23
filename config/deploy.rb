b490462180c13bf89ee412202b8e450ed6a4e8fd
nd0ut <nd0ut.me@gmail.com>
2013-10-24 03:01:26 +0400

    capistrano deploy config fixes

--- config/deploy.rb
+++ config/deploy.rb
@@ -8,16 +8,16 @@ set :scm, :git
 
 set :format, :pretty
 set :log_level, :debug
-set :pty, true
+# set :pty, true
 
 # set :linked_files, %w{config/database.yml}
 set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
 
-# set :default_env, { path: "/usr/local/rvm/bin:$PATH" }
+set :default_env, { path: "/home/rails/.rvm/bin:$PATH" }
 set :keep_releases, 1
 
 set :rvm_type, :user
-# set :rvm_ruby_version, '2.0.0@rails4'
+set :rvm_ruby_version, '2.0.0@rails4'
 set :rvm_custom_path, '/home/rails/.rvm'
 set :bundle_cmd, '/home/rails/.rvm/bin/OctoCrowd_bundle'
 
