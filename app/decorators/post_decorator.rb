128b7c5c2ec263e62f402305f59f77a630168595
nd0ut <nd0ut.me@gmail.com>
2013-10-24 12:20:17 +0400

    fix search post view

--- app/decorators/post_decorator.rb
+++ app/decorators/post_decorator.rb
@@ -23,6 +23,8 @@ class PostDecorator < Draper::Decorator
   end
 
   def search_fragment(query)
+    return nil if query.nil
+
     fragments = object.body.strip_tags.scan(/(?<=)([^.!?]+#{query}[^.!?]+)(?=(\.|!|\?))/)
     return nil if fragments.nil?
 
