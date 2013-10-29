63afe03bbc2349b96109c90ac68260884329aebe
nd0ut <nd0ut.me@gmail.com>
2013-10-29 19:14:11 +0400

    refactor routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ require 'sidekiq/web'
 
 OctoCrowd::Application.routes.draw do
   mount RedactorRails::Engine => '/redactor_rails'
+
+  # USERS
   devise_for :users, :controllers => {
     :omniauth_callbacks => "users/omniauth_callbacks",
     :registrations => "users/registrations"
@@ -9,8 +11,8 @@ OctoCrowd::Application.routes.draw do
 
   post "/auth/:provider/callback" => "authentications#create"
 
-  ActiveAdmin.routes(self)
 
+  # POSTS
   resources :posts do
     post 'comments', to: 'posts/comments#create'
     delete 'comments/:comment_id', to: 'posts/comments#destroy', as: :destroy_comment
@@ -18,19 +20,31 @@ OctoCrowd::Application.routes.draw do
 
   post 'posts/preview', to: 'posts#preview', as: :post_preview
 
+
+  # CATEGORIES
   resources :categories do
     post 'unsubscribe'
     post 'subscribe'
   end
 
+
+  # SEARCH
   get '/tag/:tag', to: 'posts#by_tag', as: :tag_search
   post '/search', to: 'posts#search', as: :search
 
-  root 'posts#index'
-  get '/page/:page', to: 'posts#index'
 
+  # ADMIN
+  ActiveAdmin.routes(self)
+
+
+  # SIDEKIQ
   authenticate :user, lambda { |u| u.admin? } do
     mount Sidekiq::Web => '/sidekiq'
   end
+
+
+  # ROOT
+  root 'posts#index'
+  get '/page/:page', to: 'posts#index'
 end
 
