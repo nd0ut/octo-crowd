e9b053321ab86c9bbcd870302e3ab6235a11394c
nd0ut <nd0ut.me@gmail.com>
2013-11-01 15:22:11 +0400

    add tags to sphinx index

--- app/indices/post_index.rb
+++ app/indices/post_index.rb
@@ -2,4 +2,6 @@ ThinkingSphinx::Index.define :post, :with => :active_record, :delta => true do
   indexes title
   indexes body
   indexes created_at, sortable: true
+
+  indexes taggings.tag.name, :as => :tags
 end
