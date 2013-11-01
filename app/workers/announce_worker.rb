e8822d6c037cb7ced6b54b946d7342255a844881
nd0ut <nd0ut.me@gmail.com>
2013-11-01 09:47:55 +0400

    install squeel (reverted from commit fb9f739a9e94c078568a62c1c4025e0f596b3b48)

--- app/workers/announce_worker.rb
+++ app/workers/announce_worker.rb
@@ -5,7 +5,7 @@ class AnnounceWorker
     post = Post.where(id: post_id).first
     return if post.nil?
 
-    post.subscriptions.includes(:user).where{user_id != my{post.author.id}}.uniq.each do |s|
+    post.subscriptions.includes(:user).where('user_id NOT IN (?)', post.author.id).uniq.each do |s|
       UserMailer.announce_post(s.user, post).deliver
     end
   end
